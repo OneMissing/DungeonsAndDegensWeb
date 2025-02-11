0b0838be3ce6da66439462a65ce7a9940c2882f3
KebabObama <lucaschyba@gmail.com>
2025-02-11 19:29:49 +0100

    change to landing page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,4 @@
-import SignOutButton from "@/components/SignOutButton";
+import SignOutButton from "@/components/signOutButton";
 export default function SignupPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-red-900 w-full">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 import userLogged from '@/lib/check';
 import { useRouter } from "next/navigation";
+const router = useRouter();
+
+const handleAuth = async () => {
+  router.push('/');
+};
 
 export default function Home() {
-  const router = useRouter();
   if(userLogged())
     router.push("/home");
   return (
     <div className="flex items-center justify-center min-h-screen bg-red-900 w-full">
         <h1>Homepage</h1>
+        <button onClick={handleAuth}></button>
     </div>
   );
 }
