016ead4c37f3edfa33480eb90d460a9986ea4fd3
Lukáš Pražák <153939275+KebabObama@users.noreply.github.com>
2025-02-11 10:49:40 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import SignupForm from '@/components/signUpForm';
 
 export default function Home() {
-    return <h1 className="text-3xl font-bold">Welcome to the Home Page!</h1>;
+    return <div></><h1 className="text-3xl font-bold">Welcome to the Home Page!</h1><signUpForm /></div>;
 }
