fcab55f2c082f34b9e9ba679ab5142bf9c53b5c3
Lukáš Pražák <153939275+KebabObama@users.noreply.github.com>
2025-02-11 10:36:13 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import Link from 'next/link';
-
+import useAuth from '@/lib/useAuth';
 export const metadata = {
   title: 'DnD',
   description: 'Playable DnD',
@@ -10,11 +10,13 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="cs">
       <body className="bg-gray-100 text-gray-900">
-        <nav className="p-4 bg-white shadow-md flex space-x-4">
-          <Link href="/" className="hover:underline">Home</Link>
-          <Link href="/autorization" className="hover:underline">Autorization</Link>
-        </nav>
-        <main className="p-6">{children}</main>
+        <useAuth>
+          <nav className="p-4 bg-white shadow-md flex space-x-4">
+            <Link href="/" className="hover:underline">Home</Link>
+            <Link href="/autorization" className="hover:underline">Autorization</Link>
+          </nav>
+          <main className="p-6">{children}</main>
+        </useAuth>
       </body>
     </html>
   );
