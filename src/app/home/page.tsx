ef9404d49d3599b6ec770cbba36aec4a8f46638b
KebabObama <lucaschyba@gmail.com>
2025-02-11 18:04:28 +0100

    ok

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,7 @@
-import SignupForm from '@/components/signUpForm';
-
 export default function SignupPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-red-900 w-full">
-      <SignupForm />
+      kkkkk
     </div>
   );
 }
