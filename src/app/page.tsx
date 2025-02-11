67e2f83b8a7748c041010a8339331b47c7294a1a
KebabObama <lucaschyba@gmail.com>
2025-02-11 21:19:37 +0100

    trY

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function PrivatePage() {
   return(
     <div className="flex items-center justify-center min-h-screen bg-red-900 w-full">
       <h1>Landing page</h1>
-      <button onClick={redirect('/')}>Get started</button>
+      <button onClick={redirect('/login')}>Get started</button>
     </div>
   );
 }
