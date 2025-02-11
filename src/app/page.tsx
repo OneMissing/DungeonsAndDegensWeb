3912c76184dae259e78bdc83de89bdd1630587c0
Lukáš Pražák <153939275+KebabObama@users.noreply.github.com>
2025-02-11 08:49:34 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import SignupForm from '@/components/signUpForm';
+
 
 export default function SignupPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <SignupForm />
+      kokot
     </div>
   );
 }
