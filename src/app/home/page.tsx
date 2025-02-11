6dab51d2e80cd28af7f56f10ffa3e77255a44d5e
KebabObama <lucaschyba@gmail.com>
2025-02-11 18:21:34 +0100

    kkk

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,7 @@
-import SignOutButton from '@/components/SignOutButton';
 export default function SignupPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-red-900 w-full">
-      <SignOutButton />
+      kkk
     </div>
   );
 }
