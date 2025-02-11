42c3e9fa79e8d6b33addaee4535f0591cd98bcd0
Lukáš Pražák <153939275+KebabObama@users.noreply.github.com>
2025-02-11 11:05:27 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { userLogged } from '@/lib/check';
+import userLogged from '@/lib/check';
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-red-900 w-full">
         <h1>Homepage</h1>
-      { userLogged }
+      { userLogged() }
     </div>
   );
 }
