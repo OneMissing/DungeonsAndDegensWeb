a59c2268299f43b96907b92ac329d73032f55f6f
Lukáš Pražák <153939275+KebabObama@users.noreply.github.com>
2025-02-11 11:22:33 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import userLogged from '@/lib/check';
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-red-900 w-full">
+    <div>
         <h1>Homepage</h1>
-      { userLogged() ?  '<nav className="p-4 bg-white shadow-md flex space-x-4">
+        { userLogged() ?  
+          '<nav className="p-4 bg-white shadow-md flex space-x-4">
             <Link href="/" className="hover:underline">Home</Link>
             <Link href="/autorization" className="hover:underline">Autorization</Link>
-          </nav>':`/autorization`}
+          </nav>'
+        :redirect(`/autorization`);}
     </div>
   );
 }
