612c62fdaf150e7d7dd667103c6502a072f1c68e
Lukáš Pražák <153939275+KebabObama@users.noreply.github.com>
2025-02-11 11:16:42 +0100

    /app layout.tsx change for check

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,11 +9,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="cs">
       <body className="bg-gray-100 text-gray-900">
-          <nav className="p-4 bg-white shadow-md flex space-x-4">
-            <Link href="/" className="hover:underline">Home</Link>
-            <Link href="/autorization" className="hover:underline">Autorization</Link>
-          </nav>
-          <main className="p-6">{children}</main>
+        <main className="p-6">{children}</main>
       </body>
     </html>
   );
