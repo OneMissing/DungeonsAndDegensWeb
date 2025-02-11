f8d3ead9a338a5ecb78707ac1ba3a51830c49fca
KebabObama <lucaschyba@gmail.com>
2025-02-11 21:23:24 +0100

    handle link fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { redirect } from 'next/navigation'
 
 export default async function PrivatePage() {
-
+  const handleLink = () => {redirect('/login')};
   return(
     <div className="flex items-center justify-center min-h-screen bg-red-900 w-full">
       <h1>Landing page</h1>
-      
+      <button onClick={handleLink()}>Get started</button>
     </div>
   );
 }
