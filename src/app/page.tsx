81fe606dcf8b35294f000d6cb681aadc6e5b7556
Lukáš Pražák <153939275+KebabObama@users.noreply.github.com>
2025-02-11 11:19:32 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-red-900 w-full">
         <h1>Homepage</h1>
-      { userLogged() ?"true":"false"}
+      { userLogged() ?  '<nav className="p-4 bg-white shadow-md flex space-x-4">
+            <Link href="/" className="hover:underline">Home</Link>
+            <Link href="/autorization" className="hover:underline">Autorization</Link>
+          </nav>':`/autorization`}
     </div>
   );
 }
