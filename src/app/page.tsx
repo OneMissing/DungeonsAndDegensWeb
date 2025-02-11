0d70b35df1159130748d22db4d9c1f2e3c6562fc
Lukáš Pražák <153939275+KebabObama@users.noreply.github.com>
2025-02-11 11:02:25 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import SignupForm from '@/components/signUpForm';
+import userLogged from '@/lib/check';
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-red-900 w-full">
         <h1>Homepage</h1>
-        <SignupForm />
+      {userLogged? "true":"false"}
     </div>
   );
 }
