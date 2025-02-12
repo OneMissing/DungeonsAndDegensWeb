99b1265aa06dbc9c8c02491f5fcde483c2e130ed
KebabObama <lucaschyba@gmail.com>
2025-02-12 19:54:55 +0100

    change of navbar to suit darker tastes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
-import Link from 'next/link';
 import './globals.css';
+import Navbar from '@/components/ui/navbar';
 export const metadata = {
   title: 'DnD',
   description: 'Playable DnD',
@@ -10,25 +10,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="cs">
       <body>
-        <nav className="bg-gradient-to-r from-red-900 via-red-700 to-red-800 text-white py-4 shadow-lg border-b-4 border-white">
-          <div className="container mx-auto flex justify-between items-center px-6">
-            <Link href="/home">
-              <a className="text-4xl font-serif font-bold text-white hover:text-yellow-400 transition duration-300">
-                Dungeons & Degens
-              </a>
-            </Link>
-
-            <div className="flex space-x-6">
-              <Link href="/login">
-                <a className="text-xl hover:text-yellow-400 transition duration-300">Home</a>
-              </Link>
-              <Link href="/about">
-                <a className="text-xl hover:text-yellow-400 transition duration-300">About</a>
-              </Link>
-
-            </div>
-          </div>
-        </nav>
+        <Navbar />
         {children}
       </body>
     </html>
---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="container mx-auto flex justify-between items-center px-6">
+        {/* Logo or Brand */}
+        <Link href="/home">
+          <a className="text-4xl font-serif font-bold text-white hover:text-yellow-400 transition duration-300">
+            Dungeons & Degens
+          </a>
+        </Link>
+
+        {/* Navigation Links */}
+        <div className="flex space-x-6">
+          <Link href="/home">
+            <a className="text-xl hover:text-yellow-400 transition duration-300">Home</a>
+          </Link>
+          <Link href="/adventure">
+            <a className="text-xl hover:text-yellow-400 transition duration-300">Adventure</a>
+          </Link>
+          <Link href="/about">
+            <a className="text-xl hover:text-yellow-400 transition duration-300">About</a>
+          </Link>
+          <Link href="/contact">
+            <a className="text-xl hover:text-yellow-400 transition duration-300">Contact</a>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
