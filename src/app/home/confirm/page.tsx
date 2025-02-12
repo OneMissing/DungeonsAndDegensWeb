0aaeaded31f56a42528c5608577313a1eec1ff10
KebabObama <lucaschyba@gmail.com>
2025-02-12 21:16:10 +0100

    Addition of confirm page for emails

---
+++ src/app/home/confirm/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function App() {
+    return (<div>You have successfully sign in!</div>);
+}
