839f9770aa895fed3d6c1b04e7650e975d1ba1ad
KebabObama <lucaschyba@gmail.com>
2025-02-12 18:19:06 +0100

    kkk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default async function PrivatePage() {
   return (
     <div className="app">
-        <nav className="bg-red-500 fixed flex justify-center z-[1000] w-full">
-                <Link href="/home" className="text-2xl text-white-900">Get started</Link>
+        <nav>
+                <Link href="/home">Get started</Link>
         </nav>
     </div>
 );
