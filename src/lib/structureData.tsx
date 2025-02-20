f1597ccb2ded387bf913d8d206e4c233378ea85c
Lukáš Pražák <153939275+KebabObama@users.noreply.github.com>
2025-02-20 11:07:54 +0100

    Update structureData.tsx to have right width and height

--- src/lib/structureData.tsx
+++ src/lib/structureData.tsx
@@ -1,8 +1,8 @@
 export const structureData = [
-    { id: "1b477e13-4e77-434e-ba52-6ab1e60ded1d", name: "Item 001", width: 50, height: 50, image_path: "/structures/item_001.png" },
-    { id: "1be303f0-f077-4a6d-bc2b-1e10f48e1221", name: "Item 002", width: 50, height: 50, image_path: "/structures/item_002.png" },
-    { id: "1063d70a-ca23-48e7-a7fd-8949249c02cc", name: "Item 003", width: 50, height: 50, image_path: "/structures/item_003.png" },
-    { id: "da72794d-86b8-45b2-9541-87539eb6fadf", name: "Item 004", width: 50, height: 50, image_path: "/structures/item_004.png" },
-    { id: "95feed38-57f8-43e5-a603-56258494f03c", name: "Item 005", width: 50, height: 50, image_path: "/structures/item_005.png" },
-    { id: "53a759a2-32e0-47aa-882f-d2c8ac2eb5c5", name: "Item 006", width: 50, height: 50, image_path: "/structures/item_006.png" },
+    { id: "1b477e13-4e77-434e-ba52-6ab1e60ded1d", name: "Item 001", width: 1, height: 2, image_path: "/structures/item_001.png" },
+    { id: "1be303f0-f077-4a6d-bc2b-1e10f48e1221", name: "Item 002", width: 1, height: 2, image_path: "/structures/item_002.png" },
+    { id: "1063d70a-ca23-48e7-a7fd-8949249c02cc", name: "Item 003", width: 1, height: 1, image_path: "/structures/item_003.png" },
+    { id: "da72794d-86b8-45b2-9541-87539eb6fadf", name: "Item 004", width: 2, height: 2, image_path: "/structures/item_004.png" },
+    { id: "95feed38-57f8-43e5-a603-56258494f03c", name: "Item 005", width: 1, height: 1, image_path: "/structures/item_005.png" },
+    { id: "53a759a2-32e0-47aa-882f-d2c8ac2eb5c5", name: "Item 006", width: 1, height: 1, image_path: "/structures/item_006.png" },
 ];
