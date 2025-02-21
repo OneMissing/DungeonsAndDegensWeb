bff7cc39660e489d74b789161d27de6edaba17dd
Lukáš Pražák <153939275+KebabObama@users.noreply.github.com>
2025-02-21 08:14:12 +0100

    Update themeToggle.tsx

--- src/components/themes/themeToggle.tsx
+++ src/components/themes/themeToggle.tsx
@@ -2,11 +2,18 @@
 
 import { useTheme } from "@/components/themes/themeProvider";
 
-export default function ThemeToggle(className: string | null) {
+interface ThemeToggleProps {
+  className?: string | null;
+}
+
+export default function ThemeToggle({ className }: ThemeToggleProps) {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button onClick={toggleTheme} className=`p-2 rounded-md bg-gray-200 dark:bg-gray-800 ${className}`>
+    <button
+      onClick={toggleTheme}
+      className={`p-2 rounded-md bg-gray-200 dark:bg-gray-800 ${className}`}
+    >
       {theme === "dark" ? "🌞 Light Mode" : "🌙 Dark Mode"}
     </button>
   );
