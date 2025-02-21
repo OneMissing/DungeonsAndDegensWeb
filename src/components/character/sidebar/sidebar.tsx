bc5d483fdf2bdbaf62dd1cfaf4bb1c2db935179c
Lukáš Pražák <153939275+KebabObama@users.noreply.github.com>
2025-02-21 12:03:43 +0100

    qqq

--- src/components/character/sidebar/sidebar.tsx
+++ src/components/character/sidebar/sidebar.tsx
@@ -9,7 +9,7 @@ const Sidebar: React.FC = () => {
     const [sidebarWidth, setSidebarWidth] = useState<number>(16);
 
     useEffect(() => {
-        const sidebarElement = document.querySelector('.sidebar') as HTMLElement | null;
+        const sidebarElement = document.getElementById("sidebar") as HTMLElement | null;
         if (sidebarElement) {
             const width = isOpen ? sidebarElement.offsetWidth : 0;
             setSidebarWidth(width);
@@ -18,26 +18,21 @@ const Sidebar: React.FC = () => {
 
     return (
         <div className="absolute z-[200] h-main">
-            {/* Sidebar */}
             <div
-                className={`sidebar flex-none transition-all duration-300 ease-in-out bg-gray-800 text-white top-0 h-main p-4`}
-                style={{ width: isOpen ? '16rem' : '0rem' }}
+                id="sidebar"
+                className={`transition-all duration-300 ease-in-out bg-gray-800 text-white top-0 h-main p-4`}
+                style={{ width: isOpen ? '16rem' : '0rem' },{display: isOpen? 'flex-none':'none'}}
             >
-                {/* Sidebar content */}
                 <ul className="space-y-4 mt-20">
                 <li><LinkCharacter /></li>
                 <li><Link href='/home/create'>Create character</Link></li>
                 </ul>
             </div>
 
-            {/* Main content */}
-                {/* Button to toggle sidebar */}
                 <button
                     className="absolute top-4 left-4 bg-gray-700 text-white rounded-full p-2 transition-all duration-300 ease-in-out"
                     onClick={() => setIsOpen(!isOpen)}
-                    style={{
-                        left: isOpen ? `calc(${sidebarWidth}px + 20px)` : '20px'
-                    }}
+                    style={{ left: isOpen ? `calc(${sidebarWidth}px + 20px)` : '20px' }}
                 >
                     {isOpen ? (
                         <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
