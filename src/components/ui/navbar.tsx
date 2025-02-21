4ad789b61dadc36830e2c292e7834691f6591deb
Lukáš Pražák <153939275+KebabObama@users.noreply.github.com>
2025-02-21 09:59:40 +0100

    adas

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { logout } from "@/lib/action";
 import { Menu, X } from "lucide-react";
-import { useAuth } from "@/lib/authContext";
+import { useAuth } from "@/components/authContext";
 import ThemeToggle from "../themes/themeToggle";
 
 const Navbar = () => {
