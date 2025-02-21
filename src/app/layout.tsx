3a0ecf1d85f86e375a0bb60b453b2dba2035e2d7
Lukáš Pražák <153939275+KebabObama@users.noreply.github.com>
2025-02-21 09:40:08 +0100

    asdad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,13 +13,14 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="cs" suppressHydrationWarning>
       <body className="h-screen overflow-hidden">
-
+        <AuthProvider>
         <ThemeProvider>
           <Navbar />
           <main className="absolute h-main top-[calc(4.5rem)] overflow-auto w-svw bg-background-light dark:bg-background-dark">
             {children}
           </main>
         </ThemeProvider>
+        </AuthProvider>
       </body>
     </html>
   );
