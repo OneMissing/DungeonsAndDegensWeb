fa56af2198b20b90d0a976a74f31f1a22f48a5bb
KebabObama <lucaschyba@gmail.com>
2025-02-22 16:59:22 +0100

    getting rid of error

--- src/app/home/level-up/[id]/page.tsx
+++ src/app/home/level-up/[id]/page.tsx
@@ -1,8 +1,8 @@
-"use client";
+/*"use client";
 
 import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
-import { getLevelUpPoints, Classes, Character, updateCharacter } from "@/lib/tools/types";
+import { Classes, Character, CharacterStat } from "@/lib/tools/types";
 import { createClient } from "@/lib/supabase/client";
 
 const LevelUpPage = () => {
@@ -48,7 +48,7 @@ const LevelUpPage = () => {
 
                 const characterClass = data.class as Classes;
                 const characterLevel = data.level ?? 1;
-                const pointsPerLevel = getLevelUpPoints(characterClass, characterLevel);
+                const pointsPerLevel = 2;
 
                 setAvailablePoints(pointsPerLevel);
 
@@ -169,4 +169,4 @@ const LevelUpPage = () => {
     );
 };
 
-export default LevelUpPage;
+export default LevelUpPage;*/
